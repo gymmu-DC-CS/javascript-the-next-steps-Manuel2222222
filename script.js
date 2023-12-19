6ddcbd194e6462ed65de86c3db0cd173c2802791
Manuel2222222 <e282248@edu.sbl.ch>
2023-12-19 16:28:14 +0100

    Did Ex. 4 and 5

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if( currentElement === "e" ) {
+    if (currentElement === "e") {
       result.push("")
-    }else if( currentElement === "E") {
-        result.push("")
-    }else {
+    } else if (currentElement === "E") {
+      result.push("")
+    } else {
       result.push(currentElement)
     }
   }
@@ -21,39 +21,64 @@ export function exercise02(args) {
   const result = []
 
   for (let i = 0; i < input.length; i++) {
-  const currentElement = input[i]
-  const capitalizedElement = currentElement.toUpperCase()
-  result.push(capitalizedElement)
-}
-return result.join("")
+    const currentElement = input[i]
+    const capitalizedElement = currentElement.toUpperCase()
+    result.push(capitalizedElement)
+  }
+  return result.join("")
 }
 
-export function exercise03(args){
+export function exercise03(args) {
   const input = args
   const result = []
   let count = 0
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if(currentElement === "e" ) {
+    if (currentElement === "e") {
       count = count + 1
-    }
-    else if (currentElement === 'E'){
+    } else if (currentElement === "E") {
       count = count + 1
     }
   }
   return count
 }
 
+export function exercise04(args) {
+  const input = args
+  let count = 1
 
-export function exercise04(args){
-    const input = args
-    const rusult = []
-    let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    const previousElement = input[i - 1]
 
-    for (let i = 0; i < input.length; i++) {
-      const currentElement = input [i]
-      if(currentElement === " ")
+    if (currentElement === " " && previousElement != "-") {
       count = count + 1
+    } else if (currentElement === "0") {
+      count = count - 1
     }
   }
+  return count
+}
+
+export function exercise05(args) {
+  const input = args
+  const result = []
+  let capitalLetters = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    const asciiCode = currentElement.charCodeAt(0)
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      return true
+    }
+  }
+
+  return false
+}
+
+export function exercise06(args) {
+  const input = args
+  const result = []
+}
